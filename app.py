3202ea0dcf1e7beace79aa6a4829cf3c77aac224
adigaboy <adiga123456@gmail.com>
2024-02-13 21:49:41 +0200

    continues input until exit command

--- app.py
+++ app.py
@@ -8,7 +8,12 @@ from crawler.crawler_cli import CrawlerCLI
 def main():
     crawler_cli = CrawlerCLI()
     loop = asyncio.get_event_loop_policy().get_event_loop()
-    loop.run_until_complete(crawler_cli.run(sys.argv[1], sys.argv[2]))
+    while True:
+        user_input = input('Please enter a url and the desired depth (type "exit" to quit):')
+        if user_input.lower() == 'exit':
+            break
+        input_text = user_input.split(' ')
+        loop.run_until_complete(crawler_cli.run(input_text[0], input_text[1]))
     loop.close()
 
 if __name__ == '__main__':
